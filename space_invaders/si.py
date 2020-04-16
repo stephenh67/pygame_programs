772f58dff7aa0fa2f8c0f891dd8b3d68efb134fe
stephenh67 <sbhope67@gmail.com>
2020-04-16 06:21:23 -0400

    add ship

--- space_invaders/si.py
+++ space_invaders/si.py
@@ -9,10 +9,29 @@ screen = pygame.display.set_mode((800, 600))
 # title and icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('images/alien.png')
+pygame.display.set_icon(icon)
+
+
+def player():
+    screen.blit(player_img, (player_x, player_y))
+
+# player
+player_img = pygame.image.load('images/rocket.png')
+player_x = 370
+player_y = 400
 
 # game loop
 running = True
 while running:
+
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
+
+    player()
+    pygame.display.update()
+
