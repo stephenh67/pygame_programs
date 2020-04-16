db4d13560b1736ee0b730be81154d9d24954fef0
stephenh67 <sbhope67@gmail.com>
2020-04-16 06:57:04 -0400

    refactor screen widths

--- space_invaders/si.py
+++ space_invaders/si.py
@@ -4,7 +4,11 @@ import pygame
 pygame.init()
 
 # create the screen
-screen = pygame.display.set_mode((800, 600))
+screen_width = 800
+screen_height = 600
+player_width = 64
+
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 # title and icon
 pygame.display.set_caption("Space Invaders")
@@ -18,16 +22,17 @@ def player(x, y):
 
 # player
 player_img = pygame.image.load('images/rocket.png')
-player_x = 370
-player_y = 400
-player_x_change = 5
+player_x = (screen_width - (player_width / 2)) / 2
+player_y = 525
+player_x_change = 0
+
 
 # game loop
 running = True
 while running:
 
     screen.fill((0, 0, 0))
-    player_x += 0.1
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -35,13 +40,15 @@ while running:
         # if keystroke is pressed check whether its right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                print("Left arrow")
+                player_x_change -= .5
             if event.key == pygame.K_RIGHT:
-                print('Right arrow')
+                player_x_change = .5
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                print("key released")
+                player_x_change = 0
+
+    player_x += player_x_change
     player(player_x, player_y)
     pygame.display.update()
 
